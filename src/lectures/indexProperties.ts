3b093124009c7493a7389f46a8017a60c6d7adfa
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-23 02:38:59 +0800

    Add indexProperties function and update user input element and error container

--- src/lectures/indexProperties.ts
+++ src/lectures/indexProperties.ts
@@ -1,3 +1,18 @@
 export default function indexProperties() {
+  const userInputElement = document.getElementById(
+    "user-input"
+  )! as HTMLInputElement;
 
+  userInputElement.value = "Hi there!";
+
+  interface ErrorContainer {
+    //[prop: id]: string;
+    [prop: string]: string;
+  }
+
+  const errorBag: ErrorContainer = {
+    //1: 'Not a valid email!',
+    email: "Not a valid email!",
+    username: "Must start with a capital character!",
+  };
 } // end of export default function indexProperties()
