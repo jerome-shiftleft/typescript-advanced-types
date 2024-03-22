59be8d78b4dfa1c495c1581afa399c1988265d29
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-23 00:30:49 +0800

    Add type guards and print employee information

--- src/lectures/typeGuards.ts
+++ src/lectures/typeGuards.ts
@@ -23,4 +23,29 @@ export default function typeGuards() {
   type Numeric = number | boolean;
 
   type Universal = Combinable & Numeric;
+
+  function add(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {
+      return `${a}${b}`;
+    }
+    return a + b;
+  }
+
+  const result = add(1, '2.5');
+  console.log(`result: ${result}`);
+
+  type UnknownEmployee = Employee | Admin;
+
+  function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log(`name: ${emp.name}`);
+    if ('privileges' in emp) {
+      console.log(`privileges: ${emp.privileges}`);
+    }
+    if('startDate' in emp) {
+      console.log(`startDate: ${emp.startDate}`);
+    }    
+  }
+
+  printEmployeeInformation(e1);
+
 } // end of export default function typeGuards()
