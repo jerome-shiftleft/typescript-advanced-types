52c935e1d800a3072600fd2d5a5eab4c9b375fb2
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-23 00:37:24 +0800

    Add type guards and useVehicle function to handle different vehicle types

--- src/lectures/typeGuards.ts
+++ src/lectures/typeGuards.ts
@@ -48,4 +48,34 @@ export default function typeGuards() {
 
   printEmployeeInformation(e1);
 
+  class Car {
+    drive() {
+      console.log('Driving...');
+    }
+  }
+
+  class Truck {
+    drive() {
+      console.log('Driving...');
+    }
+    loadCargo(amount: number) {
+      console.log(`Loading cargo... ${amount}`);
+    }
+  }
+
+  type Vehicle = Car | Truck;
+
+  const v1 = new Car();
+  const v2 = new Truck();
+
+  function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    if ('loadCargo' in vehicle) {
+      vehicle.loadCargo(1000);
+    }
+  }
+
+  useVehicle(v1);
+  useVehicle(v2);
+
 } // end of export default function typeGuards()
